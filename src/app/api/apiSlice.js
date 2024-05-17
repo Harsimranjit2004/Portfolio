7785b17f08555400e62b1b32ad0ed54a23f0aa1c
Harsimanji-singh <hsdosanjh81468@gmail.com>
2024-05-17 15:54:52 -0400

    addingUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3500",
+  baseUrl: "https://harsimranjit-singh.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
