f3467305f7c7731b7d380c34190d9fed12b520c4
Super User <root@localhost.localdomain>
2025-05-01 21:52:20 -0400

    fixing

--- src/Utils/cloundinaryUpload.js
+++ src/Utils/cloundinaryUpload.js
@@ -69,6 +69,10 @@ export default async function uploadImageToCloudinary(file) {
   const fd = new FormData();
   fd.append("file", file);
   fd.append("upload_preset", uploadPreset);
+  fd.append("use_filename", "true");         // use the uploaded file's name
+  fd.append("unique_filename", "true");      // force unique filename
+  fd.append("folder", "user-resumes");       // optional: avoid global filename conflicts
+
 
   const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`; // image ► not auto
 
